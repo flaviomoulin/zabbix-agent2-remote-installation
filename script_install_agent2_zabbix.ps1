c048f8a4bdacb9a2bbffd18a908946a88659dac6
Flávio Moulin <57070852+flaviomoulin@users.noreply.github.com>
2023-04-16 06:29:25 -0700

    Update script_install_agent2_zabbix.ps1

--- script_install_agent2_zabbix.ps1
+++ script_install_agent2_zabbix.ps1
@@ -6,7 +6,7 @@
     This script to work properly, the computer where it will be executed, must contain the "C:\zabbix_agent2" with the file "zabbix_agent2-6.4.1-windows-amd64-openssl.msi"
     You can download this file in: https://cdn.zabbix.com/zabbix/binaries/stable/6.4/6.4.1/zabbix_agent2-6.4.1-windows-amd64-openssl.msi or you can access Zabbix website and do it.
     It is necessary to change the server IP in line 44, "SERVER=172.16.20.200" to your zabbix server ip.
-    
+    You need admin profile to run this code properly
    
 .NOTES
     Authors:  Flavio M. S. Amorim, Rossano S. Bernabé
